1fb2235ddf6b4c01d4500c0ea5d45d5cf16c93af
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-11-22 13:12:32 -0500

    following connectFour article as an example, not understanding much

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log("JOE MAMA")
+function GameBoard(){
+    const columns = 3;
+    const rows = 3;
+    const board = [];
+
+    for(let i=0; i<columns; i++){
+        board[i] = []
+        for(let k=0; k<rows; k++){
+            board[i].push("x")
+        }
+    }
+
+    const GetBoard = () => board;
+
+    return GetBoard()
+}
+
+
+function Cell(){
+    let value = 0;
+    
+}
+
+console.log(GameBoard())
